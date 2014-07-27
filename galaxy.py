5753a60e461ad729116109786051d48ddaacad77
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-07-28 01:24:30 +0200

    implement the import function, to import datasets from the Galaxy history to the docker container

--- galaxy.py
+++ galaxy.py
@@ -1,6 +1,6 @@
 from bioblend import galaxy
 from bioblend.galaxy.tools import ToolClient
-#from bioblend.galaxy.histories import HistoryClient
+from bioblend.galaxy.histories import HistoryClient
 import yaml
 
 def export(filename):
@@ -11,9 +11,21 @@ def export(filename):
     tc.upload_file(filename, conf['history_id'])
 
 
-def load(id):
-    #with open('conf.yaml', 'rb') as handle:
-        #conf = yaml.load(handle)
-    #gi = galaxy.GalaxyInstance(url=conf['galaxy_url'], key=conf['api_key'])
-    # TODO: implement
-    raise Exception("Importing files from galaxy history is currently unimplemented")
+def import( dataset_id ):
+    """
+        Given the history_id that is displayed to the user, this function will
+        download the file from the history and stores it under /import/
+        Return value is the path to the dataset stored under /import/
+    """
+    with open('conf.yaml', 'rb') as handle:
+        conf = yaml.load(handle)
+
+    gi = galaxy.GalaxyInstance(url=conf['galaxy_url'], key=conf['api_key'])
+    hc = HistoryClient( gi )
+
+    file_path = '/import/%s' % dataset_id
+
+    dataset_mapping = dict( [(dataset['hid'], dataset['id']) for dataset in hc.show_history(conf['history_id'], contents=True)] )
+    hc.download_dataset(conf['history_id'], dataset_mapping[dataset_id], file_path, use_default_filename=False, to_ext=None)
+
+    return file_path
