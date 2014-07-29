c53b25a5889373ecb7995ddd513da36afd388d42
Eric Rasche <rasche.eric@yandex.ru>
2014-07-29 13:32:15 -0500

    Updated url_prefix for static content

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -1,3 +1,4 @@
+# See http://ipython.org/ipython-doc/1/interactive/public_server.html for more information.
 # Configuration file for ipython-notebook.
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
@@ -8,3 +9,4 @@ import yaml
 with open('/import/conf.yaml','r') as handle:
     conf = yaml.load(handle)
 c.NotebookApp.base_url = '/ipython/%d/' % conf['docker_port']
+c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/%d/static/' % conf['docker_port']}
