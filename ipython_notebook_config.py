2a8897a2926e5d5b069ab42b4b925969e0dc4ed0
Eric Rasche <rasche.eric@yandex.ru>
2014-07-29 10:06:15 -0500

    Docker external port accessible, used in IPyNB
    In order to configure the URL path, we need to start IPyNB with a different base URL, containing the
    external docker port. This is required for easier URL re-writing.

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -1,7 +1,10 @@
 # Configuration file for ipython-notebook.
 c = get_config()
-c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.ip = '127.0.0.1'
 c.NotebookApp.port = 6789
 c.NotebookApp.open_browser = False
 c.NotebookApp.profile = u'default'
-c.NotebookApp.base_url = '/ipython/14101/'
+import yaml
+with open('/import/conf.yaml','r') as handle:
+    conf = yaml.load(handle)
+c.NotebookApp.base_url = '/ipython/%d/' % conf['docker_port']
