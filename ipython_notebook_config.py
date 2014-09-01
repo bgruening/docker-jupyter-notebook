0dc74cee943cfb54b1fe0dd970c10be29bacfa04
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-09-01 17:05:49 +0200

    enable cross-origin resource sharing and disable x-frame-options

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -6,6 +6,8 @@ c.NotebookApp.port = 6789
 c.NotebookApp.open_browser = False
 c.NotebookApp.profile = u'default'
 c.IPKernelApp.matplotlib = 'inline'
+# This is needed for Cross-Origin Resource Sharing
+c.NotebookApp.allow_origin = '*'
 
 import os
 import yaml
@@ -18,8 +20,16 @@ if os.path.exists( config_file_path ):
         conf = yaml.load(handle)
     c.NotebookApp.base_url = '/ipython/%d/' % conf['docker_port']
     c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/%d/static/' % conf['docker_port']}
+
     if 'notebook_password' in conf:
         c.NotebookApp.password = conf['notebook_password']
 else:
     c.NotebookApp.base_url = '/ipython/'
     c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/static/'}
+
+
+# Allow the frame to be displayed within Galaxy by disabling X-Frame-Options
+#c.NotebookApp.webapp_settings = {'headers': {'X-Frame-Options': 'ALLOW-FROM http://localhost:8080'}}
+c.NotebookApp.webapp_settings['headers'] = {'X-Frame-Options': 'ALLOWALL'}
+
+
