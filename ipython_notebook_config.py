aa2aa0ab4600a7e50bd0ff9a06abc3a12c0effcb
Eric Rasche <rasche.eric@yandex.ru>
2015-04-29 10:33:26 -0500

    More switches from yaml to env

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -1,5 +1,7 @@
 # See http://ipython.org/ipython-doc/1/interactive/public_server.html for more information.
 # Configuration file for ipython-notebook.
+import os
+
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 6789
@@ -7,31 +9,25 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.profile = u'default'
 c.IPKernelApp.matplotlib = 'inline'
 
-import os
-import yaml
-
 headers = {
     'X-Frame-Options': 'ALLOWALL',
 }
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.allow_credentials = True
 
-config_file_path = '/import/conf.yaml'
 # In case this Notebook was launched from Galaxy a config file exists in /import/
 # For standalone usage we fall back to a port-less URL
-if os.path.exists( config_file_path ):
-    with open( config_file_path ,'r') as handle:
-        conf = yaml.load(handle)
-    c.NotebookApp.base_url = '/ipython/%d/' % conf['docker_port']
-    c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/%d/static/' % conf['docker_port']}
-
-    if 'notebook_password' in conf:
-        c.NotebookApp.password = conf['notebook_password']
-
-    if 'cors_origin' in conf:
-        c.NotebookApp.allow_origin = conf['cors_origin']
-else:
+if os.environ.get('DOCKER_PORT', 'none') == 'none':
     c.NotebookApp.base_url = '/ipython/'
-    c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/static/'}
+    c.NotebookApp.webapp_settings = {'static_url_prefix': '/ipython/static/'}
+else:
+    c.NotebookApp.base_url = '/ipython/%d/' % os.environ['DOCKER_PORT']
+    c.NotebookApp.webapp_settings = {'static_url_prefix': '/ipython/%d/static/' % os.environ['DOCKER_PORT']}
+
+if os.environ.get('NOTEBOOK_PASSWORD', 'none') != 'none':
+    c.NotebookApp.password = os.environ['NOTEBOOK_PASSWORD']
+
+if os.environ.get('CORS_ORIGIN', 'none') != 'none':
+    c.NotebookApp.allow_origin = os.environ['CORS_ORIGIN']
 
 c.NotebookApp.webapp_settings['headers'] = headers
