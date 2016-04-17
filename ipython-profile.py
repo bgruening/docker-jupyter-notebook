bb295558afcb6a671af2eccb9f82c7986531aaa3
Björn Grüning <bjoern@gruenings.eu>
2016-04-17 20:52:56 +0200

    Update ipython-profile.py

--- ipython-profile.py
+++ ipython-profile.py
@@ -1,3 +1,4 @@
-from galaxy_ie_helpers import get, put
-from galaxy get_galaxy_connection, _get_history_id
-HISTORY_ID = _get_history_id()
+from galaxy_ie_helpers import get
+from galaxy_ie_helpers import put
+from galaxy_ie_helpers import get_galaxy_connection
+HISTORY_ID = os.environ.get('HISTORY_ID', None)
