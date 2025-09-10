01889bb7927f4ffcc223b151b9b32304b3477f4b
Björn Grüning <bjoern@gruenings.eu>
2025-09-10 10:27:43 +0200

    try to fix CSP issues for Anne

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -12,7 +12,8 @@ headers = {
     'Content-Security-Policy':
         "; ".join([
             f"default-src 'self' https: {CORS_ORIGIN}",
-            f"img-src 'self' data: {CORS_ORIGIN}",
+            f"img-src 'self' data: blob: {CORS_ORIGIN}",
+            f"media-src http: https: file: blob: {CORS_ORIGIN}",
             f"connect-src 'self' ws://{CORS_ORIGIN_HOSTNAME}",
             f"style-src 'unsafe-inline' 'self' {CORS_ORIGIN}",
             f"script-src https: 'unsafe-inline' 'unsafe-eval' 'self' {CORS_ORIGIN}"
